0a7ccf2dadf2f9231e07cf5d017562a8753bf1fc
Josh <Joshua-Howard@users.noreply.github.com>
2020-02-06 13:44:55 -0500

    Alternative Way to Write the Child1 Component

--- Frontend/src/scenes/child1.js
+++ Frontend/src/scenes/child1.js
@@ -1,17 +1,30 @@
 import React, { useMemo } from 'react';
 import Child2 from './child2';
 
-const Child1 = ({ testProp }) => {
-  return useMemo(
-    () => (
+// const Child1 = ({ testProp }) => {
+//   return useMemo(
+//     () => (
+//       <div>
+//         Dont Re-render - Here is the prop we are passing down
+//         {testProp}
+//         <Child2 testProp={testProp} />
+//       </div>
+//     ),
+//     [testProp]
+//   );
+// };
+
+function ChildContainer({ testProp }) {
+  const Child1 = useMemo(() => {
+    return (
       <div>
         Dont Re-render - Here is the prop we are passing down
         {testProp}
         <Child2 testProp={testProp} />
       </div>
-    ),
-    [testProp]
-  );
-};
+    );
+  }, [testProp]);
 
-export default Child1;
+  return Child1;
+}
+export default ChildContainer;
