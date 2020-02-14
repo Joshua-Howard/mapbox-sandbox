6f791384e2a76051b176e8e807e0a9c89f9e0c3d
Josh <Joshua-Howard@users.noreply.github.com>
2020-02-14 11:37:51 -0500

    Test the Behavior of Props Inside of a useMemo Callback

--- Frontend/src/scenes/child1.js
+++ Frontend/src/scenes/child1.js
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useEffect } from 'react';
 import Child2 from './child2';
 
 // const Child1 = ({ testProp }) => {
@@ -14,17 +14,28 @@ import Child2 from './child2';
 //   );
 // };
 
-function ChildContainer({ testProp }) {
-  const Child1 = useMemo(() => {
-    return (
-      <div>
-        Dont Re-render - Here is the prop we are passing down
-        {testProp}
-        <Child2 testProp={testProp} />
-      </div>
-    );
-  }, [testProp]);
+// eslint-disable-next-line no-unused-vars
+function Container({ testProp }) {
+  useEffect(() => () => console.log('Child Dismounting'));
+
+  const Child1 = useMemo(
+    () => {
+      console.log('Child1 inside of the container is re-rendering');
+      return (
+        <div>
+          Dont Re-render - Here is the prop we are passing down
+          {
+            //! If the second parameter of useMemo is not updated, this testProp value will not change
+          }
+          {testProp}
+        </div>
+      );
+    },
+    []
+    // ,[testProp]  // This line will cause this function to be run each time the parent functional component is mounted
+  );
 
   return Child1;
 }
-export default ChildContainer;
+
+export default Container;
